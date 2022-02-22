6fe74ee54b309097b03f671ba92c2213a705ecdf
Stavros Kasinos <kasinos@arrival.com>
2022-02-22 18:40:08 +0000

    checkpoint

--- bin/run-flake8.sh
+++ bin/run-flake8.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-poetry run flake8 core addressbook authentication --exclude "migrations"
+poetry run flake8 core addressbook authentication --exclude "migrations" #src tests --exclude "src/base.py","src/yielding.py","src/viscoelastic.py"
